bcdca7e1f7ef7e9231d6a7cb8d9b1342fcb03f15
Gian Villegas <naigvillegas@email.com>
2026-06-06 11:00:06 +0800

    fix: persistent DB connections

--- config/db.php
+++ config/db.php
@@ -6,8 +6,12 @@ $password = getenv('DB_PASS')     ?: '';
 $port     = getenv('DB_PORT')     ?: '3306';
 
 try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password, [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_PERSISTENT         => true,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+    ]);
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
